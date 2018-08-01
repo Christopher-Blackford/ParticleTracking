bf950570ffb7ee1fc4dca920d1041f983be697df
Christopher Blackford <christopher.blackford@mail.utoronto.ca>
2018-08-01 16:54:34 -0400

    Adding figs 11-12

---
+++ zSpecies_marxan_examples_compare.R
@@ -0,0 +1,40 @@
+##############################################################################################################################
+##############################################################################################################################
+###Similarity between species
+
+#
+##
+###
+####
+#####
+#Clear workspace
+rm(list=ls())
+
+require(rgdal)
+require(rgeos)
+require(tidyverse)
+
+setwd("K:/Christopher_PhD/Github/ParticleTracking") 
+###################Initialize run with these important parameters
+
+Species_1 <- readOGR("./Marxan/To_shapefile/hexagon/Nearshore/pld3")
+Species_2 <- readOGR("./Marxan/To_shapefile/hexagon/Nearshore/pld120")
+
+Species_1 <- Species_1@data
+Species_2 <- Species_2@data
+
+Species_1$RAND <- sample(1:nrow(Species_1), nrow(Species_1), replace = FALSE)
+Species_1 <- Species_1[with(Species_1, order(-SSOLN, RAND)),]
+Species_1 <- Species_1[c(1:round(0.1*nrow(Species_1))),]
+Species_1 <- Species_1[with(Species_1, order(Poly_ID)),]
+Species_1 <- Species_1[,1]
+
+
+Species_2$RAND <- sample(1:nrow(Species_2), nrow(Species_2), replace = FALSE)
+Species_2 <- Species_2[with(Species_2, order(-SSOLN, RAND)),]
+Species_2 <- Species_2[c(1:round(0.1*nrow(Species_2))),]
+Species_2 <- Species_2[with(Species_2, order(Poly_ID)),]
+Species_2 <- Species_2[,1]
+
+
+over_lap <- length(intersect(Species_1, Species_2))/length(Species_1)
